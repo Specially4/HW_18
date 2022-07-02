eccbdc460928f289ebd4193a5a7f8f69211b74eb
I'm Blue <kotenkovsn@gmail.com>
2022-07-02 18:47:48 +0700

    added user authentication

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_restx import Api
 
 from config import Config
 from setup_db import db
+from views.user import users_ns
+from views.auth import auth_ns
 from views.director import directors_ns
 from views.genre import genres_ns
 from views.movie import movies_ns
@@ -17,6 +19,8 @@ api = Api(app)
 api.add_namespace(directors_ns)
 api.add_namespace(genres_ns)
 api.add_namespace(movies_ns)
+api.add_namespace(users_ns)
+api.add_namespace(auth_ns)
 
 if __name__ == '__main__':
     app.run()
