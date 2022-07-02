93c9b58c616c92d1a68c826c74ea49e9a03fd5ad
I'm Blue <kotenkovsn@gmail.com>
2022-07-02 18:47:35 +0700

    added user authentication

--- container.py
+++ container.py
@@ -1,9 +1,11 @@
 from dao.director import DirectorDAO
 from dao.genre import GenreDAO
 from dao.movie import MovieDAO
+from dao.user import UserDAO
 from service.director import DirectorService
 from service.genre import GenreService
 from service.movie import MovieService
+from service.user import UserService
 from setup_db import db
 
 genre_dao = GenreDAO(db.session)
@@ -15,4 +17,6 @@ director_service = DirectorService(director_dao)
 movie_dao = MovieDAO(db.session)
 movie_service = MovieService(movie_dao)
 
+user_dao = UserDAO(db.session)
+user_service = UserService(user_dao)
 
